2fa7af4ff7b34ffd50cbb49d60fa0080865a9888
unknown <sergey.v.tokareff@gmail.com>
2024-11-12 12:05:31 +0200

    edit task 1

--- Lesson_2/task_1.py
+++ Lesson_2/task_1.py
@@ -1,14 +1,22 @@
 class Editor:
     def view_document(self):
-        print(f"Перегляд документа.")
+        print(f"Ми можемо переглядати документ.")
 
     def edit_document(self):
         print(f"Редагування документів недоступне для безкоштовної версії.")
 
 class ProEditor(Editor):
     def edit_document(self):
-        print("Редагування документа.")
+        print("Ми можемо редагувати документ.")
 
-valid_license_key = "PRO"
+valid_password = "PRO12345"
 
-license_key = input("Введіть ліцензійний ключ: ")
+password = input("Введіть пароль: ")
+
+if password == valid_password:
+    obj = ProEditor()
+else:
+    obj = Editor()    
+
+obj.view_document()
+obj.edit_document()
